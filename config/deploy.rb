a0596bfa5177e280726358b3a5465b9312af9c72
Thecharmander95 <leomidili0@gmail.com>
2025-09-09 17:56:35 -0400

    changed user

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :keep_releases, 5
 set :branch, "master"
 set :application, "lionnav"
 set :repo_url, "https://github.com/Thecharmander95/lionnav.git"
-set :deploy_to, "/home/leo/Production/#{fetch :application}"
+set :deploy_to, "/home/deploy/Production/#{fetch :application}"
 append :linked_files, "config/master.key"
 
 set :rbenv_type, :user
